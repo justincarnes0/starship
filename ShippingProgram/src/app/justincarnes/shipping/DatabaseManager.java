ee4069d8868c9fb271985c5bdbc4b0a42ab98521
Justin <Marc@Downstairs-PC>
2019-07-17 17:17:29 -0500

    Working combo box, separated population of site list to getSiteList method.

--- ShippingProgram/src/app/justincarnes/shipping/DatabaseManager.java
+++ ShippingProgram/src/app/justincarnes/shipping/DatabaseManager.java
@@ -6,69 +6,117 @@ import java.io.File;
 
 public class DatabaseManager 
 {
-	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-	static final String DB_URL = "jdbc:mysql://localhost:3306/starfishcustomers";
+	static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+	static final String DB_URL = "jdbc:mysql://localhost/starfishcustomers";
 	
 	static final String USERNAME = "shipping";
 	static final String PASSWORD = "9146";
 	
 	ShippingProgramGUI gui = null;
 	
-	ArrayList<String> custKeys = new ArrayList<String>();
-	ArrayList<String> siteKeys = new ArrayList<String>();
-	ArrayList<String> accNumKeys = new ArrayList<String>();
+	ArrayList<String> custKeys = new ArrayList<String>();		//This will hold all customer primary keys
+	ArrayList<String> siteKeys = new ArrayList<String>();		//This will hold siteName for all sites of selected customer
+	ArrayList<String> accNumKeys = new ArrayList<String>();		//This will hold serviceName for all accounts of selected customer
+	
+	String activeCustomer = "";
+	String activeSite = "";
 	
 	public DatabaseManager(ShippingProgramGUI SPGui)
 	{
+		custKeys.add("--Select one--");
+		siteKeys.add("--Select one--");
+		accNumKeys.add("--Select one--");
+		
 		gui = SPGui;
 		
 		Connection conn = null;
-		Statement stmt = null;
+		Statement custStmt = null;
+		
+		Statement acctStmt = null;
 		
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName(JDBC_DRIVER);
 			
 			//TODO: Add a tooltip in bottom left of GUI window instead of printing to console
 			System.out.println("Connecting to database...");
 			conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
 			
-			System.out.println("Creating statement...");
-			stmt = conn.createStatement();
+			System.out.println("Creating statements...");
+			custStmt = conn.createStatement();
+			
+			acctStmt = conn.createStatement();
+			
 			//Will initially store primary keys. Further info to be retrieved later using PKs
 			String sql_Customer = "select custName from customer;";
-			String sql_Site = "select custName, siteName from site;";
+			
 			String sql_AccountNumber = "select custName, siteName, serviceName from accountnumber;";
 			
-			ResultSet customers = stmt.executeQuery(sql_Customer);
-			ResultSet sites = stmt.executeQuery(sql_Site);
-			ResultSet accountNums = stmt.executeQuery(sql_AccountNumber);
+			ResultSet customers = custStmt.executeQuery(sql_Customer);
+			
+			ResultSet accountNums = acctStmt.executeQuery(sql_AccountNumber);
 			
 			while(customers.next())
 				custKeys.add(customers.getString("custName"));
-			while(sites.next())
-				siteKeys.add(sites.getString("custName") + " :: " + sites.getString("siteName"));
 			while(accountNums.next())
 				accNumKeys.add(accountNums.getString("custName") + " :: " + accountNums.getString("siteName") + " :: " + accountNums.getString("serviceName"));
 			
 			customers.close();
-			sites.close();
 			accountNums.close();
-			stmt.close();
+			custStmt.close();
+			acctStmt.close();
 			conn.close();
 		} 	
 		catch(SQLException se) {
-				se.printStackTrace();}
+				se.printStackTrace(); }
 		catch(Exception e) {
-				e.printStackTrace();}	
+				e.printStackTrace(); }	
 		
 		finally {
-			try { if(stmt != null) stmt.close(); } catch(SQLException se2) {}
-			try { if(conn != null) conn.close(); } catch(SQLException se) { se.printStackTrace(); }
+			try { if(custStmt != null) custStmt.close(); } catch(SQLException se2) {}
+			
+			try { if(acctStmt != null) acctStmt.close(); } catch(SQLException se2) {}
+			try { if(conn != null) conn.close(); } catch(SQLException se)  { se.printStackTrace(); }
 		}
 	}
 	
-	public Object[] getCustKeys()
+	public Object[] getCustList()
 	{
 		return custKeys.toArray();
 	}
+	
+	public Object[] getSiteList(String custSelection)
+	{
+		activeCustomer = custSelection;
+		
+		Connection conn = null;
+		Statement siteStmt = null;
+		
+		try {
+			System.out.println("Connecting to database...");
+			conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+			
+			siteStmt = conn.createStatement();
+			String sql_Site = "select custName, siteName from site where custName=" + activeCustomer + ";";
+			ResultSet sites = siteStmt.executeQuery(sql_Site);
+			
+			while(sites.next())
+				siteKeys.add(sites.getString("siteName"));
+			
+			conn.close();
+			siteStmt.close();
+			sites.close();
+			
+		} catch(SQLException se) { se.printStackTrace(); }
+		
+		finally {
+			try { if(conn != null) conn.close(); } catch(SQLException se)  { se.printStackTrace(); }
+			try { if(siteStmt != null) siteStmt.close(); } catch(SQLException se2) {}
+		}
+		
+		return siteKeys.toArray();
+	}
+	
+	
 }
+
+
