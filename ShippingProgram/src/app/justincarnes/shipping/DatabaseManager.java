3a7333fb0a4f21a00f2c7b9aad4588fe131837c3
Justin <Marc@Downstairs-PC>
2019-07-23 18:02:46 -0500

    Lots of updates to both classes.
    ==ShippingProgramGUI==
    -Added the site ComboBox, pops up once a customer is selected in the
    first CB. Still need to figure out how to not create a new one when the
    selection changes.
    
    -Moved all code relating to the creation of CBs into their own methods.
    Constructor now simply creates the application frame and calls
    "createCustomersBox".
    
    -Added an event listener to the customers CB, when the box's state is
    changed the selection is sent to the "createSitesBox", which then
    queries the relevant sites from the DBM and creates a new CB.
    
    
    ==DatabaseManager==
    -Moved all code relating to querying and populating lists into their own
    methods. Constructor now solely registers the driver and calls the
    "getCustList" method.
    
    -As mentioned, created "getCustList", "getSiteList", and
    "getAccountList" methods. Pretty much what they say on the tin.
    
    -A ton of comments. Still need to do that for the GUI class.

--- ShippingProgram/src/app/justincarnes/shipping/DatabaseManager.java
+++ ShippingProgram/src/app/justincarnes/shipping/DatabaseManager.java
@@ -4,89 +4,93 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.io.File;
 
+//A class to manage the MySQL database containing Starfish customer information
+//Fetches information from the database
+//Adds new info to the database
+//Creates backup files to rebuild the database in case of data loss
 public class DatabaseManager 
 {
-	static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-	static final String DB_URL = "jdbc:mysql://localhost/starfishcustomers";
+	static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";	//Driver package
+	static final String DB_URL = "jdbc:mysql://localhost/starfishcustomers";	//URL for database: for now, locally hosted
 	
+	//Database credentials
 	static final String USERNAME = "shipping";
 	static final String PASSWORD = "9146";
 	
-	ShippingProgramGUI gui = null;
+	ShippingProgramGUI gui = null;	//Holds the current GUI instance
 	
-	ArrayList<String> custKeys = new ArrayList<String>();		//This will hold all customer primary keys
-	ArrayList<String> siteKeys = new ArrayList<String>();		//This will hold siteName for all sites of selected customer
-	ArrayList<String> accNumKeys = new ArrayList<String>();		//This will hold serviceName for all accounts of selected customer
+	ArrayList<String> custKeys = new ArrayList<String>();	//This will hold all customer primary keys
+	ArrayList<String> siteKeys = new ArrayList<String>();	//This will hold siteName for all sites of selected customer
+	ArrayList<String> accNumKeys = new ArrayList<String>();	//This will hold serviceName for all accounts of selected customer
 	
-	String activeCustomer = "";
-	String activeSite = "";
+	String activeCustomer = "";		//Keeps track of what customer is currently selected in the GUI combobox
+	String activeSite = "";			//Keeps track of which site is currently selected in the GUI combobox
 	
+	//Constructor for the database manager object
+	//Currently accepts the active GUI instance, ideally console messages will be shown in the window in the future
 	public DatabaseManager(ShippingProgramGUI SPGui)
 	{
-		custKeys.add("--Select one--");
-		siteKeys.add("--Select one--");
-		accNumKeys.add("--Select one--");
-		
 		gui = SPGui;
-		
-		Connection conn = null;
-		Statement custStmt = null;
-		
-		Statement acctStmt = null;
-		
-		try {
+		try {	//to register the driver and retrieve the list of customers
 			Class.forName(JDBC_DRIVER);
-			
-			//TODO: Add a tooltip in bottom left of GUI window instead of printing to console
-			System.out.println("Connecting to database...");
-			conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
-			
-			System.out.println("Creating statements...");
-			custStmt = conn.createStatement();
-			
-			acctStmt = conn.createStatement();
-			
-			//Will initially store primary keys. Further info to be retrieved later using PKs
-			String sql_Customer = "select custName from customer;";
-			
-			String sql_AccountNumber = "select custName, siteName, serviceName from accountnumber;";
-			
-			ResultSet customers = custStmt.executeQuery(sql_Customer);
-			
-			ResultSet accountNums = acctStmt.executeQuery(sql_AccountNumber);
-			
-			while(customers.next())
-				custKeys.add(customers.getString("custName"));
-			while(accountNums.next())
-				accNumKeys.add(accountNums.getString("custName") + " :: " + accountNums.getString("siteName") + " :: " + accountNums.getString("serviceName"));
-			
-			customers.close();
-			accountNums.close();
-			custStmt.close();
-			acctStmt.close();
-			conn.close();
-		} 	
-		catch(SQLException se) {
-				se.printStackTrace(); }
-		catch(Exception e) {
-				e.printStackTrace(); }	
-		
-		finally {
-			try { if(custStmt != null) custStmt.close(); } catch(SQLException se2) {}
-			
-			try { if(acctStmt != null) acctStmt.close(); } catch(SQLException se2) {}
-			try { if(conn != null) conn.close(); } catch(SQLException se)  { se.printStackTrace(); }
+			getCustList();
 		}
+		catch(Exception e) { e.printStackTrace(); }	//If there is a problem registering the driver
 	}
 	
+	//A method to fetch the customer list from the MySQL database
 	public Object[] getCustList()
 	{
-		return custKeys.toArray();
+		//If something is already in the list, flush it
+		if(!custKeys.isEmpty()) 
+			custKeys.clear();		
+		
+		custKeys.add("--Select one--");	//Gives the combobox a default value, will be an invalid selection
+		
+		Connection conn = null;			//Will represent a connection with the database
+		Statement custStmt = null;		//An object that will execute queries in the database
+		try {
+			//Follows URL to database, provides credentials for access
+			System.out.println("Connecting to database...");
+			conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);		
+			
+			custStmt = conn.createStatement();			
+			String sql_Customer = "select custName from customer;";
+			ResultSet customers = custStmt.executeQuery(sql_Customer);	//Takes the above query and executes it in the DB
+			
+			//Iterates through result set and adds all "custName" values to the "custKeys" AList
+			while(customers.next())
+				custKeys.add(customers.getString("custName"));
+			
+			conn.close(); 		//Closes all database resources: these are not local so they will not be destroyed by garbage collection
+			custStmt.close();	//Connections, statements, and result sets are representations of database objects, and are thus held in the DB
+			customers.close();	//If we don't close after opening, they will stay open in the DB indefinitely even after the Java variables are gone
+			
+		} catch(SQLException se) { se.printStackTrace(); }		//If there is some error with the SQL query
+		
+		finally {	//If an exception is thrown and nothing happens, we must still close the DB resources
+			try { if(conn != null) conn.close(); } catch(SQLException se)  { se.printStackTrace(); }
+			try { if(custStmt != null) custStmt.close(); } catch(SQLException se2) {}
+			System.out.println("DB connection terminated.");
+		}
+		
+		return custKeys.toArray();	//ComboBox can only accept arrays, not ALists
 	}
 	
-	public Object[] getSiteList(String custSelection)
+	//A method to fetch the site list for the customer selected
+	//Will never be run before getCustList, only called when a selection is made in the customer ComboBox
+	public Object[] getSiteList(String custSelection)	
 	{
-		activeCustomer = custSelection;
+		activeCustomer = custSelection;	//Saves the customer that the user currently has selected
+		
+		//Fail-safe if somehow "--Select one--" makes it here or customer has not been selected
+		//Neither scenario should ever happen, but you know
+		if(activeCustomer.equals("--Select one--") || activeCustomer.equals("")) return new Object[1];
+		
+		if(!siteKeys.isEmpty())
+			siteKeys.clear();
+		
+		siteKeys.add("--Select one--");
 		
 		Connection conn = null;
 		Statement siteStmt = null;
@@ -96,7 +100,8 @@ public class DatabaseManager
 			conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
 			
 			siteStmt = conn.createStatement();
-			String sql_Site = "select custName, siteName from site where custName=" + activeCustomer + ";";
+			//This SQL statement must select only sites for the selected customer
+			String sql_Site = "select custName, siteName from site where custName='" + activeCustomer + "';";
 			ResultSet sites = siteStmt.executeQuery(sql_Site);
 			
 			while(sites.next())
@@ -111,11 +116,58 @@ public class DatabaseManager
 		finally {
 			try { if(conn != null) conn.close(); } catch(SQLException se)  { se.printStackTrace(); }
 			try { if(siteStmt != null) siteStmt.close(); } catch(SQLException se2) {}
+			System.out.println("DB connection terminated.");
 		}
 		
 		return siteKeys.toArray();
 	}
 	
+	//A method to fetch the account information for the selected site
+	//Will never be called before customer or site are selected
+	public Object[] getAccountList(String siteSelection)
+	{
+		activeSite = siteSelection;
+		
+		//Fail-safe if somehow "--Select one--" makes it to cust/site, or cust/site has not been selected
+		//Neither scenario should ever happen, but you know
+		if(activeCustomer.equals("--Select one--") || activeCustomer.equals("") || activeSite.equals("--Select one--") || activeSite.equals(""))
+			return new Object[1];
+		
+		if(!accNumKeys.isEmpty())
+			accNumKeys.clear();
+	
+		accNumKeys.add("--Select one--");
+		
+		Connection conn = null;
+		Statement acctStmt = null;
+		
+		try {
+			//TODO: Add a tooltip in bottom left of GUI window instead of printing to console
+			System.out.println("Connecting to database...");
+			conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+			acctStmt = conn.createStatement();
+			
+			//Will initially store primary keys. Further info to be retrieved later using PKs
+			//Only select keys for selected customer/site combo
+			String sql_AccountNumber = "select custName, siteName, serviceName from accountnumber where custName='" + activeCustomer + "' and siteName='" + activeSite + "';";
+			ResultSet accountNums = acctStmt.executeQuery(sql_AccountNumber);
+			while(accountNums.next())
+				accNumKeys.add(accountNums.getString("serviceName"));
+			
+			conn.close();
+			acctStmt.close();
+			accountNums.close();
+			
+		} 	catch(SQLException se) { se.printStackTrace(); }
+		
+		finally {
+			try { if(acctStmt != null) acctStmt.close(); } catch(SQLException se2) {}
+			try { if(conn != null) conn.close(); } catch(SQLException se)  { se.printStackTrace(); }
+			System.out.println("DB connection terminated.");
+		}
+		
+		return accNumKeys.toArray();
+	}
 	
 }
 
--- ShippingProgram/src/app/justincarnes/shipping/ShippingProgramGUI.java
+++ ShippingProgram/src/app/justincarnes/shipping/ShippingProgramGUI.java
@@ -9,11 +9,21 @@ import javax.swing.border.EmptyBorder;
 import java.awt.FlowLayout;
 import javax.swing.JLabel;
 import javax.swing.JComboBox;
+import java.awt.event.ItemListener;
+import java.awt.event.ItemEvent;
+import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.DefaultComboBoxModel;
 
 public class ShippingProgramGUI extends JFrame 
 {
 
 	private JPanel contentPane;
+	private DatabaseManager dbm;
+	private JComboBox comboBox_Customers;
+	private JComboBox comboBox_Sites;
 
 	//Launches program
 	public static void main(String[] args) 
@@ -38,21 +48,51 @@ public class ShippingProgramGUI extends JFrame
 	//Creates the application frame
 	public ShippingProgramGUI() 
 	{
-		DatabaseManager dbm = new DatabaseManager(this);				//Creates a database manager to interface with MySQL
+		dbm = new DatabaseManager(this);								//Creates a database manager to interface with MySQL
 		
 		setTitle("Starfish Shipping");									//Creates the label at the top of the window
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);					//Behavior when closing program
-		setBounds(100, 100, 450, 300);									//Sets default window size
+		setBounds(100, 100, 500, 300);									//Sets default window size
 		contentPane = new JPanel();										//Creates a content pane
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));				//Content pane's border
-		setContentPane(contentPane);									//Attaches content pane to frame
-		contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));	//Pane in flow layout
-		
-		JLabel lblSelectACustomer = new JLabel("Select a customer");	//A label for the combo box
-		contentPane.add(lblSelectACustomer);							//Adds label to the content pane
-		
-		JComboBox comboBox = new JComboBox(dbm.getCustList());			//Creates a combo box for the customer list
-		contentPane.add(comboBox);										//Adds combo box to content pane
+		setContentPane(contentPane);
+		contentPane.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
+		createCustomersBox();
 	}
-
+	
+	private void createCustomersBox()
+	{
+		JLabel lblSelectACustomer = new JLabel("Select a customer");
+		contentPane.add(lblSelectACustomer);							
+		
+		comboBox_Customers = new JComboBox(dbm.getCustList());
+		comboBox_Customers.setModel(new DefaultComboBoxModel(new String[] {"--Select one--", "Aero Controls & DLE Consultants", "Aero Land & Marine", "Albioma Galion", "Alkhorayef Petroleum", "Allied Power Technologies", "APR Energy", "Arizona Public Service", "Arkansas Electric Cooperative Corporation", "Arnet Cleanpower", "Austin Energy", "Aviation Power & Marine", "Baseload Power", "BasicEParts", "Basin Electric Power Cooperative", "Belize Electricity Limited", "Black & Veatch", "Black Hills", "Brownsville Public Utility Board", "Bryan Texas Utilities", "Calpine Operating Services Co", "City of Medicine Hat", "City Water & Light (Jonesboro)", "Consumers Energy Company", "Coolidge Power", "CPS Energy", "CPV Sentinel", "Craig International", "Craig International Supplies", "DCP Midstream", "DEMEC", "Didrikson Associates", "DM Lieferant", "Dominion Energy", "DXP Enterprises", "East Kentucky Power Cooperative", "El Paso Electric Company", "Elliott Ebara Turbomachinery Corporation", "ELPower Trading", "Enesur/Adenty S.A.", "EthosEnergy", "EthosEnergy Australia", "Evolution Well Service", "Gamatech S.A.", "Gas Turbine Services", "Global IGT Solutions", "Hawaii Electric Light Company", "Hoosier Energy Rural Electric Cooperative", "I.S.R. Service Co.", "Instrumentation & Control Spares", "International Alliance Group", "LM Parts", "Los Angeles Department of Water and Power", "Louisiana Energy & Power Authority", "Lubbock Power & Light", "Madison Gas & Electric Company", "Maximum Turbine Support Europe", "Meritek Inc.", "Midwest Paper Group", "MSU Energy", "MTU Maintenance", "Municipal Light & Power", "New York Power Authority", "NRG Energy Center", "Oklahoma Municipal Power Authority", "Overseas Aviation", "Peru LNG S.R.L.", "Pinnacle Parts and Service Corporation", "Pio Pico Energy Center", "ProEnergy Services", "Ratchaburi World Cogeneration", "Siemens Energy", "SJ Turbine", "Sociedad Austral de Generacion y Energia Chile", "South Texas Electric Cooperative", "Starfish PPS", "Streamline Parts & Tooling", "Talen (Topaz Power Group)", "Texas A&M University", "The University of Texas at Austin", "Thermal Energy Corporation", "Toyo Thai Power Myanmar", "TransCanada", "University Park Energy", "V&A Hi-Tech", "Valve & Instrument Express", "VBR Turbine Partners", "WDF Aviation Services", "Western Farmers Electric Cooperative", "Wildflower Energy"}));
+		contentPane.add(comboBox_Customers);
+		
+		comboBox_Customers.addItemListener(new ItemListener() 
+		{
+			public void itemStateChanged(ItemEvent custSelectedEvent) 
+			{
+				String selection = (String) custSelectedEvent.getItem();
+				if(selection.equals("--Select one--"))
+					return;
+				else createSitesBox(selection);
+			}
+		});
+	}
+	
+	private void createSitesBox(String selection)
+	{
+		JLabel lblSelectASite = new JLabel("Select a site");
+		lblSelectASite.setBounds(57, 59, 58, 14);
+		contentPane.add(lblSelectASite);
+		
+		comboBox_Sites = new JComboBox(dbm.getSiteList(selection));
+		comboBox_Sites.setBounds(120, 56, 251, 20);
+		
+		contentPane.add(comboBox_Sites);
+		contentPane.validate();
+		contentPane.repaint();
+		
+	}
 }
